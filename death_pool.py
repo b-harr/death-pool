cd0bf9310a771c7ed7582310d1a00a069fe91e60
B. Harrison <cbharrison@utexas.edu>
2025-12-25 04:28:23 -0600

    Change order of libraries.

--- death_pool.py
+++ death_pool.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
 
